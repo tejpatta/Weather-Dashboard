d8df163e7475e43604e09d4d33c5bfb0756f086b
Tejpatta <tejurawal@gmail.com>
2024-01-22 11:09:54 +0000

    add logic to append bootstrap cards for weather forecast

--- script.js
+++ script.js
@@ -42,10 +42,10 @@ function displayCurrentWeather(url){
         return response.json();
     })
     .then(function(data){
-        console.log(data);
-    let cityName = data.name
+    let cityName = data.name;
     let currentDate = dayjs().format('DD MMM YYYY');
-    let icon = 'https://openweathermap.org/img/wn/'+weatherIcon+'@2x.png'
+    let icon = data.weather[0].icon;
+    let iconURL = 'https://openweathermap.org/img/wn/'+ icon +'@2x.png'
     let currentTemperature = Math.round(data.main.temp)
     let currentWind = data.wind.speed
     let currentHumidity = data.main.humidity
@@ -55,7 +55,7 @@ function displayCurrentWeather(url){
         $('<div class="card p-3 m-2 border-warning">').append([
             $('<div class="card-header">').append(
                 $('<h2>').text(cityName + ' '+ currentDate).append(
-                $('<img>').attr('src', icon))), 
+                $('<img>').attr('src', iconURL))), 
                 $('<div class="card-body">').append([
                 $('<p>').text('Temperature: '+ currentTemperature +'°C'),
                  $('<p>').text('Humidity: '+ currentHumidity +'%'),
@@ -74,25 +74,39 @@ function displayForecast(url){
         return response.json();
     })
     .then(function(data){
-    console.log(data);
-        /*
-        // FOR LOOP HERE for( var i=0; i<LENGTH; i++)
-        {
-            let displayDate =
-            let icon =
-            let temperature =
-            let wind =
-            let humidity =
-            //CREATE DIVS TO DUMP INFO IN
-        }*/
-    })
-}
+        forecast.append($('<h2> 5 days forecast </h2>'))
+
+        for(let i=0; i<data.list.length; i=i+8){
+
+            let dispayDate = dayjs(data.list[i].dt_txt).format('DD MMM YYYY');
+            let icon = data.list[i].weather[0].icon;
+            let iconURL = 'https://openweathermap.org/img/wn/'+ icon +'@2x.png';
+            let temperature = Math.round(data.list[i].main.temp);
+            let humidity = data.list[i].main.humidity;
+            let windSpeed = data.list[i].wind.speed;
+            
+            forecast.append(
+                $('<div class="card forecast p-2 m-2 border-warning" style="width: 12rem;">').append([
+                $('<div class="card-header">').append([
+                        $('<h4>').text(dispayDate),
+                        $('<img>').attr('src', iconURL)]), 
+                $('<div class="card-body">').append([
+                        $('<p>').text('Temperature: '+ temperature +'°C'),
+                        $('<p>').text('Humidity: '+ humidity +'%'),
+                        $('<p>').text('Wind Speed: '+ windSpeed +'kmph') 
+                     ])
+                ])
+            )
+        }
+    }
+)}
 
 
 //event listeners to run the functions
 
 $("#search-button").on("click", function(event){
 event.preventDefault();
-clearView()
+clearView();
+generateCityButton();
 dashboard(); 
 })
