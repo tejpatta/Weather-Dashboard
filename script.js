4411a59fe2134bd8bff813ca5c7f8ebfa2a1697f
Tejpatta <tejurawal@gmail.com>
2024-01-23 18:47:34 +0000

    change structure of API and function calls, pass the correct parameter into dashboard function

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 // take in the city name input typed by the user
-let citySearched = $("#search-input").val().trim();
+
+let key = '1aff72a595bc5c38e3d0296bf6ef9274'
 //initialize latitude and longitude variables to store coordinates of City Search
 let lat =""
 let lon =""
+let today = $('#today')
+let forecast = $('#forecast')
 
-//define API URLS
-let geocodeURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + 'London' + '&limit=1&appid=1aff72a595bc5c38e3d0296bf6ef9274'
+
 
 // functions
 // save the search term as a button
 function generateCityButton(){
     
 }
-// get the latitude and longitude of the city searched 
-function dashboard(){
-    fetch(geocodeURL)
+function dashboard(url){
+    fetch(url)
     .then(function (response) {
         return response.json();
       })
       .then(function(data){
     lat = data[0].lat;
     lon = data[0].lon;
-    let currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=1aff72a595bc5c38e3d0296bf6ef9274';
+    let currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key;
       displayCurrentWeather(currentWeatherURL);
-    let forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=1aff72a595bc5c38e3d0296bf6ef9274';
+    let forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + key;
         displayForecast(forecastURL);
     
     })
@@ -42,17 +43,18 @@ function displayCurrentWeather(url){
         return response.json();
     })
     .then(function(data){
+        console.log(data);
     let cityName = data.name;
     let currentDate = dayjs().format('DD MMM YYYY');
     let icon = data.weather[0].icon;
     let iconURL = 'https://openweathermap.org/img/wn/'+ icon +'@2x.png'
-    let currentTemperature = Math.round(data.main.temp)
-    let currentWind = data.wind.speed
-    let currentHumidity = data.main.humidity
+    let currentTemperature = Math.round(data.main.temp);
+    let currentWind = data.wind.speed;
+    let currentHumidity = data.main.humidity;
 
 
     today.append(
-        $('<div class="card p-3 m-2 border-warning">').append([
+        $('<div class="card p-3 m-2 border-success">').append([
             $('<div class="card-header">').append(
                 $('<h2>').text(cityName + ' '+ currentDate).append(
                 $('<img>').attr('src', iconURL))), 
@@ -74,26 +76,26 @@ function displayForecast(url){
         return response.json();
     })
     .then(function(data){
-        forecast.append($('<h2> 5 days forecast </h2>'))
+        forecast.append($('<h2> 5 Day Forecast </h2>'))
 
         for(let i=0; i<data.list.length; i=i+8){
 
-            let dispayDate = dayjs(data.list[i].dt_txt).format('DD MMM YYYY');
-            let icon = data.list[i].weather[0].icon;
-            let iconURL = 'https://openweathermap.org/img/wn/'+ icon +'@2x.png';
+            let displayDate = dayjs(data.list[i].dt_txt).format('DD MMM YYYY');
+            let forecastIcon = data.list[i].weather[0].icon;
+            let forecastIconURL = 'https://openweathermap.org/img/wn/'+ forecastIcon +'@2x.png';
             let temperature = Math.round(data.list[i].main.temp);
             let humidity = data.list[i].main.humidity;
             let windSpeed = data.list[i].wind.speed;
             
             forecast.append(
-                $('<div class="card forecast p-2 m-2 border-warning" style="width: 12rem;">').append([
+                $('<div class="card forecast p-2 m-2 border-success" style="width: 12rem;">').append([
                 $('<div class="card-header">').append([
-                        $('<h4>').text(dispayDate),
-                        $('<img>').attr('src', iconURL)]), 
+                        $('<h4>').text(displayDate),
+                        $('<img>').attr('src', forecastIconURL)]),
                 $('<div class="card-body">').append([
                         $('<p>').text('Temperature: '+ temperature +'°C'),
                         $('<p>').text('Humidity: '+ humidity +'%'),
-                        $('<p>').text('Wind Speed: '+ windSpeed +'kmph') 
+                        $('<p>').text('Wind Speed: '+ windSpeed +'kmph'),
                      ])
                 ])
             )
@@ -104,9 +106,10 @@ function displayForecast(url){
 
 //event listeners to run the functions
 
-$("#search-button").on("click", function(event){
-event.preventDefault();
-clearView();
-generateCityButton();
-dashboard(); 
+$('#search-button').click(function(event){
+    let citySearched = $("#search-input").val().trim(); 
+    let geocodeURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + citySearched + '&limit=1&appid=' + key
+    event.preventDefault();
+    clearView();
+ dashboard(geocodeURL);
 })
